a58c13e9566e0aa49d65cb9878f0b9637bf9f84b
Steve Karaga <stephenkaraga@gmail.com>
2023-01-23 22:30:37 -0500

    Create Tag module.

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get } from "@nestjs/common";
+import { TagEntity } from "./tag.entity";
+import { TagService } from "./tag.service";
 
-@Controller()
+@Controller('tags')
 export class TagController {
+    constructor(private readonly tagService: TagService) {}
     @Get()
-    findAll() {
-        return ['dragons', 'coffee']
+    async findAll(): Promise<TagEntity[]> {
+        return await this.tagService.findAll();
     }
 }
